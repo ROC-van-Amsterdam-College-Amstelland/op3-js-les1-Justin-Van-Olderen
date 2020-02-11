39490de93eb28261bbfcea63e07638848b3cd2db
Justin van Olderen <2107471@talnet.nl>
2020-02-11 15:53:38 +0100

    Commit 1/10

--- opdracht01/js/script.js
+++ opdracht01/js/script.js
@@ -4,7 +4,9 @@ var go = "groen";
 
 window.onload = function(){
     //JOUW CODE HIER
-
-    
+    document.getElementById("light-top").innerHTML  = "rood";
+    document.getElementById("light-middle").innerHTML = "oranje";
+    document.getElementById("light-bottom").innerHTML ="groen";
+   
 }
 
